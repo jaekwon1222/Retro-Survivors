07e642ec99df216c79af85114dd04a6546944f82
Julianizceja <julianizceja@csu.fullerton.edu>
2025-11-21 23:22:16 -0800

    Added Melee with Manual Option

---
+++ Assets/Script/Cotrollers/WeaponMeleeParent.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class WeaponMeleeParent : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public SpriteRenderer characterRenderer, weaponRenderer;
+    public Vector2 PointerPosition { get; set; }
+
+    public Animator animator;
+    public float delay = 0.3f;
+    private bool attackBlocked;
+
+    public bool IsAttacking { get; private set; }
+
+    public void ResetIsAttacking()
+    {
+        IsAttacking = false;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (IsAttacking)
+            return;
+        Vector2 direction = (PointerPosition - (Vector2)transform.position).normalized;
+        transform.right = direction;
+
+        Vector2 scale = transform.localScale;
+        if (direction.x < 0)
+        {
+            scale.y = -1;
+        }
+        else if (direction.x > 0)
+        {
+            scale.y = 1;
+        }
+        transform.localScale = scale;
+
+        if(transform.eulerAngles.z > 0 && transform.eulerAngles.z < 180)
+        {
+            weaponRenderer.sortingOrder = characterRenderer.sortingOrder - 1;
+        }
+        else
+        {
+            weaponRenderer.sortingOrder = characterRenderer.sortingOrder + 1;
+        }
+    }
+
+    public void Attack()
+    {
+        if (attackBlocked)
+            return;
+        animator.SetTrigger("Attack");
+        IsAttacking = true; 
+        attackBlocked = true;
+        StartCoroutine(DelayAttack());
+    }
+
+    private IEnumerator DelayAttack()
+    {
+        yield return new WaitForSeconds(delay);
+        attackBlocked = false;
+    }
+}
